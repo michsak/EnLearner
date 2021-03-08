07faf8310ba88785f2ff97f3235ea13da6a4d88c
Michał Sakowicz <michalsakowicz99@gmail.com>
2021-03-08 03:50:16 +0100

    added server parser class, affixed icons

--- app/src/main/java/com/project/enlearner/MainActivity.java
+++ app/src/main/java/com/project/enlearner/MainActivity.java
@@ -1,21 +1,26 @@
 package com.project.enlearner;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
+
+import com.parse.CountCallback;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 //TODO
-//take random word from parser
 //website-scraping and showing in textbox
 //read words from database - database config
 //alert how many days
 
 public class MainActivity extends WearableActivity
 {
+    private String word = "";
+    private final int wordsCount = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,33 @@ public class MainActivity extends WearableActivity
         setAmbientEnabled();
 
         startService(new Intent(this, BackgroundService.class));
+
+        downloadWordsFromDataBase();
+    }
+
+    private void downloadWordsFromDataBase()
+    {
+        int randomWordNumber = (int)(Math.random()*(wordsCount));
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Words");
+        query.orderByAscending("word");
+        query.setSkip(randomWordNumber);
+        query.getFirstInBackground(new GetCallback<ParseObject>()
+        {
+            @Override
+            public void done(ParseObject object, ParseException e)
+            {
+                if (e==null && object!=null)
+                {
+                    word = object.getString("word").toString();
+                    Button button = findViewById(R.id.button2);
+                    button.setText(word);
+                }
+                else
+                {
+                    Log.i("word", "fail");
+                }
+            }
+        });
     }
 
     public void onWordClicked(View view)
