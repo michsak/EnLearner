83ba15574af20b5e98040f327738b12dcc0ff62e
Michał Sakowicz <michalsakowicz99@gmail.com>
2021-03-14 00:10:13 +0100

    add functionality to scrape examples

--- web_scraper/src/CambridgeDictionaryScraper.java
+++ web_scraper/src/CambridgeDictionaryScraper.java
@@ -7,44 +7,59 @@ import java.io.IOException;
 
 public class CambridgeDictionaryScraper
 {
-    private static final int tableSize = 3;
-    private static final String word = "aggravate";
+    private static final int scrapingTableSize = 2;
+    private static final String word = "complicit";
 
     public static void main (String[] args)
     {
-        String[] text = scrapeAllFromPage(String.format("https://dictionary.cambridge.org/pl/dictionary/english/%s", word));
-        String[] clearText = new String[tableSize];
+        String[] definitionText = scrapeFromPage(String.format("https://dictionary.cambridge.org/pl/dictionary/english/%s", word), "def ddef_d db");
+        String[] examplesText = scrapeFromPage(String.format("https://dictionary.cambridge.org/pl/dictionary/english/%s", word), "examp dexamp");
+        String[] clearDefinitionText = new String[scrapingTableSize];
+        String[] clearExamplesText = new String[scrapingTableSize];
 
-        for (int i=0; i<tableSize; i++)
+        for (int i = 0; i< scrapingTableSize; i++)
         {
-            clearText[i] = removeAllRedundantAttributes(text[i]);
+            clearDefinitionText[i] = removeAllRedundantAttributes(definitionText[i]);
+            clearExamplesText[i] = removeAllRedundantAttributes(examplesText[i]);
         }
 
-        if (clearText[0] != null && !clearText[0].equals("") && !clearText[0].startsWith("past "))
+        if (clearDefinitionText[0] != null && !clearDefinitionText[0].equals("") && !clearDefinitionText[0].startsWith("past "))
         {
-            System.out.println(clearText[0]);
+            System.out.println(clearDefinitionText[0]);
         }
+
         else
         {
-            System.out.println(clearText[1]);
+            System.out.println(clearDefinitionText[1]);
+        }
+
+        if (clearExamplesText[0] != null && !clearExamplesText[0].equals(""))
+        {
+            System.out.println(clearExamplesText[0]);
+        }
+
+        else
+        {
+            System.out.println(clearExamplesText[1]);
         }
     }
 
-    public static String[] scrapeAllFromPage(String link)
+
+    private static String[] scrapeFromPage(String link, String divClassName)
     {
-        String[] rawDefinition = new String[tableSize];
+        String[] rawDefinition = new String[scrapingTableSize];
 
         try
         {
             Document doc = Jsoup.connect(link).get();
-            Elements allContent = doc.getElementsByClass("def ddef_d db");
+            Elements allContent = doc.getElementsByClass(divClassName);
 
             int i = 0;
             for (Element seperateLine : allContent)
             {
                 rawDefinition[i] = seperateLine.toString();
                 i += 1;
-                if (i >= tableSize) {break; }
+                if (i >= scrapingTableSize) {break; }
             }
         }
         catch (IOException e)
@@ -55,12 +70,14 @@ public class CambridgeDictionaryScraper
         return rawDefinition;
     }
 
+
     private static String removeAllRedundantAttributes(String text)
     {
         String finalText = "";
         if (text != null && !text.equals(""))
         {
             String message = Jsoup.clean(text, Whitelist.none());
+            finalText = message;
 
             if (message.endsWith(":"))
             {
